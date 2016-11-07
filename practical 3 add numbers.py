99ef0ae100d11b9f7766e6c7aaea8db4c1913439
jonathanwmfj <jonathanwmfj@gmail.com>
2016-11-07 16:13:48 +0000

    Practical 3. Adding numbers together

---
+++ practical 3 add numbers.py
@@ -0,0 +1,9 @@
+#two floats to be added together
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+#adding float 1 and float 2 to find sum
+sum = float(num1) + float(num2)
+
+#The sum of float 1 and float 2
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
