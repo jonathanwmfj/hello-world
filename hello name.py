dd6e7f7463e02eab977c25b99650d5f222c5ddbc
jonathanwmfj <jonathanwmfj@gmail.com>
2016-11-07 15:09:49 +0000

    practical session 2, Task 5

---
+++ hello name.py
@@ -0,0 +1,4 @@
+#whatever is entered by the user is stored in a variable called 'name'
+name = input("Hi there, What is your name? ")
+#here you can see the + is used to add strings together
+print ("Hello " + name + " How are you?")
